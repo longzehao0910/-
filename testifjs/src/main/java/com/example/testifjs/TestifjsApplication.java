1b3fd0a71f890fa545777bfd89b1617be95e5933
longzehao0910 <506446451@qq.com>
2020-07-22 19:12:29 +0800

    项目第3次上传

---
+++ testifjs/src/main/java/com/example/testifjs/TestifjsApplication.java
@@ -0,0 +1,13 @@
+package com.example.testifjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestifjsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestifjsApplication.class, args);
+    }
+
+}
---
+++ testifjs/src/test/java/com/example/testifjs/TestifjsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testifjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestifjsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
