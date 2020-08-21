555bdda782b58fefb9ade4b9f61c4a7469d5ddef
longzehao0910 <506446451@qq.com>
2020-08-21 14:32:49 +0800

    bush commit jt-mall

--- jt-mall/sp02-itemservice/src/main/java/com/example/sp02itemservice/Sp02ItemserviceApplication.java
+++ jt-mall/sp02-itemservice/src/main/java/com/example/sp02itemservice/Sp02ItemserviceApplication.java
@@ -2,7 +2,9 @@ package com.example.sp02itemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class Sp02ItemserviceApplication {
 
