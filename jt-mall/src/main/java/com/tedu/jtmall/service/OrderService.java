02f3a13715adcaa4f6f4f02f219b6f2ab9cd3eb1
longzehao0910 <506446451@qq.com>
2020-10-26 14:43:44 +0800

    2020/10/26 尝试更改提交jt-mall

--- jt-mall/src/main/java/com/tedu/jtmall/service/OrderService.java
+++
@@ -1,9 +0,0 @@
-package com.tedu.jtmall.service;
-
-import com.tedu.jtmall.pojo.Order;
-
-public interface OrderService {
-    Order getOrder(String orderId);
-
-    void addOrder(Order order);
-}
--- jt-mall/src/main/java/com/tedu/jtmall/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.tedu.jtmall.service;
-
-import com.tedu.jtmall.pojo.User;
-
-public interface UserService {
-    User getUser(Integer id);
-
-    void addScore(Integer id, Integer score);
-}
